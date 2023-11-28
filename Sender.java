cf445b1525e100bad1fb6cf5ea547227987f3bff
Wang0John <john-wang@hotmail.fr>
2023-11-28 14:41:53 +0100

    add tout

--- Sender.java
+++ Sender.java
@@ -19,52 +19,28 @@ public class Sender {
         final String user = getSenderName(args); 
 
         try {
-            ArrayList<String> ip = new ArrayList<String>();
+            CarnetAdresse ca = new CarnetAdresse();
+
             String message = new String();
-
-            FileInputStream list_fis = new FileInputStream("IP.txt");
-            InputStreamReader list_isr = new InputStreamReader(list_fis);
-            BufferedReader list_br = new BufferedReader(list_isr);
-
-            while(list_br.readLine() != null) {
-                ip.add(list_br.readLine());
-            }
-
-            list_br.close();
 
             InputStreamReader isr = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(isr);
 
-            for (String s : ip) {
-                Socket so = new Socket(s,2023);
+            for (int i = 0; i < ca.getSize(); i++ ) {
+                Socket so = new Socket(ca.getIp(i),2023);
 
                 OutputStream os = so.getOutputStream();
                 OutputStreamWriter osw = new OutputStreamWriter(os);
                 BufferedWriter bw = new BufferedWriter(osw);
 
                 message = br.readLine();
-
-                bw.write(user);
-                bw.newLine();
-                bw.flush();
-                System.out.println(user);
-
                 LocalDateTime ldt = LocalDateTime.now();
                 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                bw.write(ldt.format(dtf));
-                bw.newLine();
-                bw.flush();
-                System.out.println(ldt.format(dtf));
 
-                bw.write(String.valueOf(message.length()));
-                bw.newLine();
+                Message m = new Message(user, ldt.format(dtf), String.valueOf(message.length()), message);
+                bw.write(m.send());
                 bw.flush();
-                System.out.println(message.length());
-
-                bw.write(message);
-                bw.flush();
-                System.out.println(message);
-
+                System.out.println(m.send());
                 so.close();
             }
         }
