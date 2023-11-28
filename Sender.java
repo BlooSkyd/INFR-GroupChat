66a76a45117a06474c55d5c30d8ca4eec7c2e22a
BlooSkyd <clment2402@gmail.com>
2023-11-28 11:11:21 +0100

    gitignore and basic arg interpreter

--- Sender.java
+++ Sender.java
@@ -1,5 +1,13 @@
 public class Sender {
     public static void main(String[] args) {
-        
+        System.out.println(Sender.getSenderName(args));
+    }
+
+    private static String getSenderName(String[] args) {
+        String res = "";
+        for (String arg : args) {
+            res += arg;
+        }
+        return res;
     }
 }
